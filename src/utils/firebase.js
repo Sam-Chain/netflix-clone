dc60328f0c5565a456d5653c1ee5d980ed41c78e
sam-chain <merah.saddam@gmail.com>
2022-04-15 02:46:24 +0100

    set up firebase to deploy

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,15 @@
-import firebase from 'firebase'
-const firebaseApp = firebase.initializeApp({
+import { initializeApp } from 'firebase/app';
 
-});
 
-const db = firebaseApp.firestore();
-export default db;
+const firebaseConfig = {
+    apiKey: "AIzaSyBh19zS_7YJ-QUDd1Kud2gnoE2_zGqpItQ",
+    authDomain: "netflix-clone-go.firebaseapp.com",
+    databaseURL: "https://netflix-clone-go.firebaseio.com",
+    projectId: "netflix-clone-go",
+    storageBucket: "netflix-clone-go.appspot.com",
+    messagingSenderId: "103023310838",
+    appId: "1:103023310838:web:e57a321dc42471bf54f9d7",
+    measurementId: "G-XRBRPM4508"
+};
+
+const app = initializeApp(firebaseConfig);
